81f41074fad7b7c993a1ba9ef0309ed055e8479d
joonamo <joonamo@users.noreply.github.com>
2023-02-19 19:04:08 +0200

    Looks like a working solution

--- pages/api/configuration.ts
+++ pages/api/configuration.ts
@@ -1,13 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { runCors } from '@/src/cors'
 import { getLatestYear } from '@/src/miku-scrape'
 import { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handler(
-  _: NextApiRequest,
+  req: NextApiRequest,
   res: NextApiResponse
 ) {
+  await runCors(req, res)
+
   res.json({
     firstYear: 2012,
-    lastYear: await getLatestYear()
+    latestYear: await getLatestYear()
   })
 }
--- pages/api/popular.ts
+++ pages/api/popular.ts
@@ -1,4 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { runCors } from '@/src/cors'
 import { popularTag, processPage } from '@/src/miku-scrape'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
@@ -6,6 +7,8 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  await runCors(req, res)
+
   const year = String(req.query?.['year'] ?? '2020')
   const page = Number(req.query?.['page'] ?? 1)
   res.json(await processPage(year, popularTag, page))
