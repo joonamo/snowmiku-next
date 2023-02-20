7ff08b455030298ea465ad78cb50c98ec8d1077f
joonamo <joonamo@users.noreply.github.com>
2023-02-20 20:56:45 +0200

    Show loader when changing pages

--- components/App.tsx
+++ components/App.tsx
@@ -33,14 +33,31 @@ export interface AppProps {
 }
 
 export const App: React.FC<Partial<AppProps>> = (props) => {
-  const { isFallback } = useRouter()
-  console.log(props)
+  const router = useRouter()
+  
+  const [loading, setLoading] = React.useState(false)
+
+  React.useEffect(() => {
+    const handleStart = (url: string) => (url !== router.asPath) && setLoading(true);
+    const handleComplete = (url: string) => (url === router.asPath) && setLoading(false);
+
+    router.events.on('routeChangeStart', handleStart)
+    router.events.on('routeChangeComplete', handleComplete)
+    router.events.on('routeChangeError', handleComplete)
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart)
+      router.events.off('routeChangeComplete', handleComplete)
+      router.events.off('routeChangeError', handleComplete)
+    }
+  })
+
 
   return <MikuPage 
     configuration={props.configuration ?? null}
     currentPage={props.currentPage ?? 1}
     imagesInfos={props.imagesInfos ?? []}
-    isLoading={isFallback}
+    isLoading={router.isFallback || loading}
     pageCount={props.pageCount ?? 1}
     viewMode={props.viewMode ?? defaultViewMode}
     year={props.year ?? null}
