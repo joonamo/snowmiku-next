3e0892123d1d0ee4ef1f19dd67de9f4727de5360
Joona Heinikoski <joonamo@users.noreply.github.com>
2024-02-13 15:27:50 +0200

    Update 2025 theme

--- src/metadatabase.ts
+++ src/metadatabase.ts
@@ -16,7 +16,7 @@ export const metadatabase: Record<string, YearMetadata> = {
     finalists: [],
     officialPage: 'https://piapro.jp/pages/official_collabo/2025snowmiku_a4suj/',
     theme: 'キラキラ​スノー​マテリアル',
-    themeTranslated: 'Glitter Snow Material',
+    themeTranslated: 'Glittering Snow Material',
   },
   '2024': {
     winner: 'H6zW',
