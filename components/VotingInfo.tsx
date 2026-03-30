3ccf852794b5ed41034e2908b744cddbae225c6b
joonamo <joonamo@users.noreply.github.com>
2026-03-30 21:18:48 +0300

    Add voting info for Snow Miku 2027

--- components/VotingInfo.tsx
+++ components/VotingInfo.tsx
@@ -1,14 +1,14 @@
 import * as React from 'react'
 
-export const VotingInfo = () => null
+// export const VotingInfo = () => null
 
-export const VotingInfoUnused = () => (
+export const VotingInfo = () => (
   <div className='card has-background-danger-light has-text-light mb-5'>
     <div className='card-content'>
       <p className='subtitle has-text-centered'>
-        Voting for Snow Miku 2026 is open 28th March ~ 7th April 2025.{' '}
+        Voting for Snow Miku 2027 is open March 30th ~ April 9th 2026.{' '}
         <span>
-          <a href='https://event.goodsmile.info/s4slffc/en/' target='blank'>
+          <a href='https://info.goodsmile.com/sp/C7M1k3jC/en/' target='blank'>
             {/* eslint-disable-next-line no-irregular-whitespace */}
             Vote here!{' '}
           </a>
